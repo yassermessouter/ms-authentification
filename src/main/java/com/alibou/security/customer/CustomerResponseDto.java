dadf7e750c0ef6dcda7e9dd76c5183c3df9fea63
yassermessouter <105636024+yassermessouter@users.noreply.github.com>
2024-06-03 17:52:29 +0100

    Update CustomerResponseDto.java

--- src/main/java/com/alibou/security/customer/CustomerResponseDto.java
+++ src/main/java/com/alibou/security/customer/CustomerResponseDto.java
@@ -14,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 public class CustomerResponseDto {
     private Integer id;
+    private String token;
     private String companyName;
     private String fullName;
     private String email;
