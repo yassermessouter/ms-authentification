e8bd4f3eda5b7c9e5eb972f7094765ca31367366
Mohamed Ouksili <medouksili@gmail.com>
2024-06-09 13:20:41 +0100

    fix bug

--- src/main/java/com/alibou/security/role/RoleController.java
+++ src/main/java/com/alibou/security/role/RoleController.java
@@ -1,6 +1,8 @@
 package com.alibou.security.role;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,7 +39,5 @@ public class RoleController {
         String errorMessage = e.getMessage();
         return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
-
-
-
+    
 }
